1174c9cbc29b59cb0f7a0436ee7177864c39eec4
Amanda Charbonneau <charbo24@msu.edu>
2017-08-25 18:11:22 -0400

    third r script not running right

--- scripts/1_RunPhenotypicAnalysis.R
+++ scripts/1_RunPhenotypicAnalysis.R
@@ -18,4 +18,17 @@ packages(rmarkdown)
 
 rmarkdown::render('../scripts/1.1_MarkerPaperSetup.Rmd', output_file = '../Figures/MarkerPaperSetup.html')
 rmarkdown::render('../scripts/1.2_PhenotypicAnalysis.Rmd', output_file = '../Figures/PhenotypicAnalysis.html')
+
+rm(list = ls())
+
+packages<-function(x){
+  x<-as.character(match.call()[[2]])
+  if (!require(x,character.only=TRUE)){
+    install.packages(pkgs=x,repos="http://cran.r-project.org")
+    require(x,character.only=TRUE)
+  }
+}
+packages(knitr)
+packages(rmarkdown)
+
 rmarkdown::render('../scripts/1.3_SummaryStatistics.Rmd', output_file= '../Figures/SummaryStatistics.html')
